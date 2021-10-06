f5a998248b1760eaf477e9ee5f47109ff6699cbb
Tom4sBeltr4n <tomasfelipe29@hotmail.com>
2021-10-06 14:09:46 -0500

    Clase 7 acabada

--- Taller_1/Figuras.js
+++ Taller_1/Figuras.js
@@ -2,31 +2,37 @@
 // Objetivo: ser capaz de crear una
 
 /// Cuadrado
-const ladCua = 5;
-const perCua = 4 * ladCua;
-const áreCua = ladCua**2;
-console.log("El lado del cuadrado son " + ladCua + " centímetros, por lo que su perímetro son " + perCua + " centímetros y su área es " + áreCua + " centímetros cuadrados.");
+function perCua(ladCua)
+{
+    return ladCua *4
+};
+function áreCua (ladCua)
+{
+    return ladCua**2
+};
 
 /// Triángulo
-console.group("Triángulo");
-const ladTri1 = 6;
-const ladTri2 = 7;
-const ladTri3 = 5;
-console.log("Los lados del triángulo miden: " + ladTri1 + ", " + ladTri2 + " y " + ladTri3 + " centímetros")
-const perTri = ladTri3 + ladTri2 + ladTri1;
-console.log("El perímetro del triángulo es " + perTri);
-const semPerTri = perTri/2;
-const áreTri = Math.sqrt(semPerTri*(semPerTri-ladTri1)*(semPerTri-ladTri2)*(semPerTri-ladTri3));
-console.log("El área del triángulo es aproximadamente " + Math.floor(áreTri));
-console.groupEnd();
+function perTri(ladTri1, ladTri2, ladTri3)
+{
+    return ladTri1+ladTri2+ladTri3;
+};
+
+function áreTri(ladTri1,ladTri2,ladTri3)
+{
+    var vperTri=perTri(ladTri1, ladTri2, ladTri3);
+    var semPerTri=vperTri/2;
+    return Math.sqrt(semPerTri*(semPerTri-ladTri1)*(semPerTri-ladTri2)*(semPerTri-ladTri3)).toFixed(2);
+};
+
 /// Círculo
-console.group("Círculo");
-const radCír = 4;
-const diáCír = radCír * 2;
-const pi = Math.PI;
-const perCír = diáCír*pi;
-const areCír = (radCír**2)*pi;
-console.log("El radio del círculo es de " + radCír + " centímetros, su diámetro de " + diáCír + " centímetros");
-console.log("El perímetro del círculo es de apróximadamente " + perCír.toFixed(2) + " centímetros, y su área de apróximadamente " + areCír.toFixed(2) + " centímetros cuadrados");
-console.groupEnd();
-console.log(Math)
+var pi = Math.PI;
+
+function perCír(radCír)
+{
+    const diáCír = radCír * 2;
+    return diáCír*pi;
+};
+function areCír(radCír)
+{
+    return (radCír*pi)
+};
