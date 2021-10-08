6e16a62bc0e651776fecaf59eac1589c8b02c07d
Tom4sBeltr4n <tomasfelipe29@hotmail.com>
2021-10-08 12:59:33 -0500

    Clase 12 terminada; sigue el reto

--- Taller_2/Porcentajes.js
+++ Taller_2/Porcentajes.js
@@ -1,7 +1,9 @@
-const preOri = 120;
-const des = 18;
-
-function preDes(preOri, des)
+function calDes()
 {
-    return preOri*(1-(des/100));
+    const inpPri = document.getElementById("inpPri");
+    const preOri = inpPri.value;
+    const inpDes = document.getElementById("inpDes");
+    const des = inpDes.value;
+    const resSpa = document.getElementById("preFin");
+    resSpa.innerHTML = "El precio con descuento es de " + (preOri*(1-(des/100))).toFixed(2) + " dólares";
 };
