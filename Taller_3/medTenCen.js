d62c95b8000499ea286bb636940b38ded0c76026
Tom4sBeltr4n <tomasfelipe29@hotmail.com>
2021-10-13 13:34:31 -0500

    Reto de la clase 16 completado

--- Taller_3/medTenCen.js
+++ Taller_3/medTenCen.js
@@ -33,10 +33,23 @@ if (esPar(list.length))
     const elemento1 = list[middleList-1];
     const elemento2 = list[middleList];
     const mediana = calPro([elemento1, elemento2]);
-    console.log(mediana);
 } else 
 {
     const mediana = list[middleList-0.5];
-    console.log(mediana);
 }
 //reto1: hacer función de mediana, hacer que la función organice arreglos desordenados y array.sort()
+
+function calMed(lista)
+{
+    lista.sort(function(a,b){return a-b});
+    if(lista.length%2==0)
+    {
+        const index = (lista.length)/2;
+        const resultado = (lista[index]+lista[index-1])/2;
+        return resultado;
+    } 
+    else {
+        const resultado = lista[((lista.length)/2)-0.5];
+        return resultado;
+    }
+};
