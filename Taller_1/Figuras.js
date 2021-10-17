9cca131b8d19673f75ba15484f3ca5996aee6616
Tom4sBeltr4n <tomasfelipe29@hotmail.com>
2021-10-17 13:04:18 -0500

    Orden de variables en la función de altura de triángulo, problemas de ám bito resueltos y comentados, orden d e métodos del arreglo lados, borré c omentarios

--- Taller_1/Figuras.js
+++ Taller_1/Figuras.js
@@ -2,36 +2,30 @@
 // Objetivo: ser capaz de crear una
 
 /// Cuadrado. Usaremos este par de funciones para conectar con HTML en la clase 8
-var inpCua = document.getElementById("ladCua");
-var valLadCua = inpCua.value;
 
-function perCua()
+function perCua()//USE PARÁMETROS PARA PODER EVITAR PROBLEMAS DE ÁMBITO. Aunque introducir la variable dentro de la función puede ayudar ya que así se actualizarían las cosas
 {
+    var inpCua = document.getElementById("ladCua");
+    var valLadCua = inpCua.value;
     let texResPerCua=document.getElementById("valPerCua");
     texResPerCua.innerHTML=(valLadCua*4);
 };
-function áreCua ()
+function áreCua()
 {
+    var inpCua = document.getElementById("ladCua");
+    var valLadCua = inpCua.value;
     let texResÁreCua=document.getElementById("valÁreCua");
     texResÁreCua.innerHTML=(valLadCua**2);
 };
 
-/// Triángulo
-// var inpTri1 = document.getElementById("ladTri1");
-// var valLadTri1 = parseInt(inpTri1.value);
-// var inpTri2 = document.getElementById("ladTri2");
-// var valLadTri2 = parseInt(inpTri2.value);
-// var inpTri3 = document.getElementById("ladTri3");
-// var valLadTri3 = parseInt(inpTri3.value);
-
 function altTri()
 {
     var inpTri1 = document.getElementById("ladTri1");
-    var valLadTri1 = parseInt(inpTri1.value);
+    var valLadTri1 = parseFloat(inpTri1.value);
     var inpTri2 = document.getElementById("ladTri2");
-    var valLadTri2 = parseInt(inpTri2.value);
+    var valLadTri2 = parseFloat(inpTri2.value);
     var inpTri3 = document.getElementById("ladTri3");
-    var valLadTri3 = parseInt(inpTri3.value);
+    var valLadTri3 = parseFloat(inpTri3.value);
     console.log("Lado 1: "+valLadTri1+"\n Lado 2: "+valLadTri2+"\n Lado 3: "+valLadTri3)
     if(valLadTri1!=valLadTri2 && valLadTri3!=valLadTri1 && valLadTri3!=valLadTri2)//Descartando triángulos escalenos
     {
@@ -41,18 +35,18 @@ function altTri()
         alert("Este triángulo es equilátero, no operaré con él")
     } else
     {
+        let texResAltTri=document.getElementById("valAltTri");
         let lados = [];
         lados.push(valLadTri1);
         lados.push(valLadTri2);
         lados.push(valLadTri3);
-        lados.sort(function(a,b){return a-b});
-        let texResAltTri=document.getElementById("valAltTri");
+        lados.sort(function(a,b){return a-b});// don't use arrow functions as meþods
         if(lados[0]==lados[1])
         {
             var altura = (Math.sqrt((lados[1]**2)-(((lados[2])/2)**2))).toFixed(2);
         }else
         {
-            var altura = (Math.sqrt((lados[0]**2)-(((lados[1])/2)**2))).toFixed(2);
+            var altura = (Math.sqrt((lados[1]**2)-(((lados[0])/2)**2))).toFixed(2);
         }
         texResAltTri.innerHTML=altura;
     }
